9a2bbe2246bc905f585d4cff82b0b99f2b9dabdd
jerry <calljerryli@outlook.com>
2023-08-01 14:18:43 +0800

    write id,sponsor,drug to csv

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import json
+import csv
 import requests
+
+result_set = set()
 
 
 def get_study_json_by_id(id: str):
@@ -23,9 +26,33 @@ with open('CRPC.json') as f:
 crpc_hits = crpc['hits']
 # print(crpc_hits)
 
+with open('other.CRPC.json') as f:
+    other_crpc = json.load(f)
+# print(other_crpc)
+
+other_crpc_hits = other_crpc['hits']
+# print(other_crpc_hits)
+
+crpc_hits += other_crpc_hits
+
 for item in crpc_hits:
     id = item['id']
-    # print(id)
+    print(id)
+
+    protocol_section = item['study']['protocolSection']
+
+    sponsor = protocol_section['sponsorCollaboratorsModule']['leadSponsor']['name']
+
+    drug_list = []
+    if 'armsInterventionsModule' in protocol_section:
+        arms_interventions_module = protocol_section['armsInterventionsModule']
+        if 'interventions' in arms_interventions_module:
+            interventions = arms_interventions_module['interventions']
+            for item in interventions:
+                type = item['type']
+                if type == 'DRUG':
+                    drug_list.append(item['name'])
+    drug_list_str = ','.join(drug_list)
 
     study_json = get_study_json_by_id(id)
     eligibility_module = study_json['study']['protocolSection']['eligibilityModule']
@@ -33,4 +60,9 @@ for item in crpc_hits:
     # print(eligibility_criteria)
 
     if str(eligibility_criteria).find('enzalutamide') != -1 or str(eligibility_criteria).find('abiraterone') != -1:
-        print(id)
+        result_set.add((id, sponsor, drug_list_str))
+
+with open('data.csv', 'w') as f:
+    write = csv.writer(f)
+    write.writerow(['id', 'sponsor', 'drug'])
+    write.writerows(result_set)
